92300f7d5ac34fe90e7c804c956604bcf7775d24
Elise <elisegamassa@gmail.com>
2019-06-19 16:46:10 +0200

    done

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+Recipe.create({
+  title: "cookies",
+  level: "Easy Peasy",
+  ingredient: [
+    "salted butter",
+    "sugar",
+    "brow sugar",
+    "vanilla",
+    "eggs",
+    "flour",
+    "baking soda",
+    "chocolate"
+  ],
+  cuisine: "american",
+  dishType: "Snack",
+  duration: 60,
+  creator: "Elise"
+})
+  .then(dbRes => {
+    console.log(dbRes.title);
+  })
+  .catch(err => console.log(err));
+
+Recipe.insertMany(data)
+  .then(dbRes => {
+    console.log(dbRes);
+  })
+  .catch(err => console.log(err));
+
+Recipe.updateOne({ title: "Rigatoni alla Genovese" }, { duration: 100 })
+  .then(dbRes => {
+    console.log(dbRes);
+    console.log("The Rigatoni alla Genovese have been updated");
+  })
+  .catch(err => console.log(err));
+
+Recipe.deleteOne({ title: "Carrot Cake" })
+  .then(dbRes => {
+    console.log("The Carrot Cake have been removed");
+  })
+  .catch(err => console.log(err));
+
+mongoose.connection.close();
